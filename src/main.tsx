e6742b8506f41d41d590fef3e61249d42bb37643
GlennOdoom <148824030+GlennOdoom@users.noreply.github.com>
2025-05-11 19:58:40 +0000

    proof

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './Styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./Styles/Index.css";
 //import { AuthContext } from './Contexts/AuthenticationContext'
-import { AuthProvider } from './Contexts/AuthenticationContext'
-
+import { AuthProvider } from "./Contexts/AuthenticationContext";
 
 // Initialize user state
-const authValue = {
-  user: null,
-  setUser: () => {}
-};
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
